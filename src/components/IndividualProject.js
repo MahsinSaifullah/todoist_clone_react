17a28bc87b0ab028d5d4551ed925a0f2fea60dca
macbookpro <mahsinsaifullah@gmail.com>
2020-03-10 15:26:49 +0000

    main funtionality developed

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -4,66 +4,60 @@ import { useProjectsValue, useSelectedProjectValue } from '../context';
 import { firebase } from '../firebase';
 
 export const IndividualProject = ({ project }) => {
-  const [showConfirm, setShowConfirm] = useState(false);
-  const { projects, setProjects } = useProjectsValue();
-  const { setSelectedProject } = useSelectedProjectValue();
+	const [showConfirm, setShowConfirm] = useState(false);
+	const { projects, setProjects } = useProjectsValue();
+	const { setSelectedProject } = useSelectedProjectValue();
 
-  const deleteProject = docId => {
-    firebase
-      .firestore()
-      .collection('projects')
-      .doc(docId)
-      .delete()
-      .then(() => {
-        setProjects([...projects]);
-        setSelectedProject('INBOX');
-      });
-  };
+	const deleteProject = docId => {
+		firebase
+			.firestore()
+			.collection('projects')
+			.doc(docId)
+			.delete()
+			.then(() => {
+				setProjects([...projects]);
+				setSelectedProject('INBOX');
+			});
+	};
 
-  return (
-    <div className='individual-project'>
-      <span className="sidebar__dot">•</span>
-      <span className="sidebar__project-name">{project.name}</span>
-      <span
-        className="sidebar__project-delete"
-        data-testid="delete-project"
-        onClick={() => setShowConfirm(!showConfirm)}
-        onKeyDown={() => setShowConfirm(!showConfirm)}
-        tabIndex={0}
-        role="button"
-        aria-label="Confirm deletion of project"
-      >
-        <FaTrashAlt />
-      </span>
-      {showConfirm && (
-          <div className="project-delete-modal">
-            <div className="project-delete-modal__inner">
-                <div>
-                     <p>Are you sure you want to delete this project?</p>
-                </div>
-                <div>
-                    <button
-                    type="button"
-                    onClick={() => deleteProject(project.docId)}
-                    >
-                         Delete
-                     </button>
-                    <span
-                        onClick={() => setShowConfirm(!showConfirm)}
-                        onKeyDown={() => setShowConfirm(!showConfirm)}
-                        tabIndex={0}
-                        role="button"
-                        aria-label="Cancel adding project, do not delete"
-                    >
-                        Cancel
-                    </span>
-                </div>
-             
-              
-            </div>
-          </div>
-        )}
-    </div>
-  );
+	return (
+		<div className='individual-project'>
+			<span className='sidebar__dot'>•</span>
+			<span className='sidebar__project-name'>{project.name}</span>
+			<span
+				className='sidebar__project-delete'
+				data-testid='delete-project'
+				onClick={() => setShowConfirm(!showConfirm)}
+				onKeyDown={() => setShowConfirm(!showConfirm)}
+				tabIndex={0}
+				role='button'
+				aria-label='Confirm deletion of project'>
+				<FaTrashAlt />
+			</span>
+			{showConfirm && (
+				<div className='project-delete-modal'>
+					<div className='project-delete-modal__inner'>
+						<div>
+							<p>Are you sure you want to delete this project?</p>
+						</div>
+						<div>
+							<button
+								type='button'
+								onClick={() => deleteProject(project.docId)}>
+								Delete
+							</button>
+							<span
+								onClick={() => setShowConfirm(!showConfirm)}
+								onKeyDown={() => setShowConfirm(!showConfirm)}
+								tabIndex={0}
+								role='button'
+								aria-label='Cancel adding project, do not delete'>
+								Cancel
+							</span>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
-
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,50 +1,43 @@
 import React, { useEffect } from 'react';
 import { Checkbox } from './Checkbox';
+import { AddTask } from './AddTask';
 import { useTasks } from '../hooks';
 import { collatedTasks } from '../constants';
 import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
 import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
-  const { selectedProject } = useSelectedProjectValue();
-  const { projects } = useProjectsValue();
-  const { tasks } = useTasks(selectedProject);
+	const { selectedProject } = useSelectedProjectValue();
+	const { projects } = useProjectsValue();
+	const { tasks } = useTasks(selectedProject);
 
-  let projectName = '';
+	let projectName = '';
 
-  if (collatedTasksExist(selectedProject) && selectedProject) {
-    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
-  }
+	if (collatedTasksExist(selectedProject) && selectedProject) {
+		projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+	}
 
-  if (
-    projects &&
-    selectedProject &&
-    !collatedTasksExist(selectedProject)
-  ) {
-    projectName = getTitle(projects, selectedProject).name;
-  }
+	if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+		projectName = getTitle(projects, selectedProject).name;
+	}
 
-  useEffect(() => {
-    document.title = `${projectName}: Todoist`;
-  });
+	useEffect(() => {
+		document.title = `${projectName}: Todoist`;
+	});
 
-  return (
-    <div className="tasks" data-testid="tasks">
-      <h2 data-testid="project-name">{projectName}</h2>
+	return (
+		<div className='tasks' data-testid='tasks'>
+			<h2 data-testid='project-name'>{projectName}</h2>
 
-      <ul className="tasks__list">
-        {tasks.map(task => (
-          <li key={`${task.id}`}>
-            <Checkbox id={task.id} taskDesc={task.task} />
-            <span>{task.task}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+			<ul className='tasks__list'>
+				{tasks.map(task => (
+					<li key={`${task.id}`}>
+						<Checkbox id={task.id} taskDesc={task.task} />
+						<span>{task.task}</span>
+					</li>
+				))}
+			</ul>
+			<AddTask />
+		</div>
+	);
 };
-
-
-
-
-
